21b652eb8590556a9dbc2883231cc43f0cef620e
eAkti <ecem.topallar@gmail.com>
2017-06-13 15:38:57 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# A script to set up a new mac.
+
+# Helpers
+function msg { echo -e '\033[1;32m'"$1"'\033[0m'; }
+function warn { echo -e '\033[1;33m'"$1"'\033[0m'; }
+function error { echo -e '\033[1;31mERROR: '"$1"'\033[0m'; }
+
+msg "Install starting. You may be asked for your password (for sudo)."
+
+# Homebrew
+if hash brew &> /dev/null; then
+    msg "Homebrew already installed"
+else
+    warn "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    warn "Homebrew is installed"
+fi
+
+# Installing Apps from Brewfile
+warn "Installing Apps..."
+brew bundle --file=resources/Brewfile
+
+# Update Profile
+cp ./resources/.bash_profile ~/.bash_profile
+
+# Cleanup
+brew cleanup && brew doctor
+
+msg "Done."
